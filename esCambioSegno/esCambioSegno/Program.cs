7cef3a2f7d5b0f0725d620b24d3af358f9ba4056
Loren-France <14529846@itisrossi.vi.it>
2026-09-13 22:40:52 +0200

    Esercizio ripasso 2 contatore dei cambi di segno

---
+++ esCambioSegno/esCambioSegno/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Schema;
+
+
+namespace esCambioSegno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool control;
+
+            Console.WriteLine("PROGRAMMA CAMBIO SEGNO");
+
+            Console.WriteLine("Inserisci quanti numeri vuoi inserire:");
+
+            int n;
+            int cont = 0;
+
+            do
+            {
+                control = int.TryParse(Console.ReadLine(), out n);
+                
+                if (!control || n <= 0)
+                {
+                    Console.WriteLine("Errore: inserisci un numero intero positivo.");
+                }
+            }
+            while (!control || n <= 0);
+
+            int[] numeri = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Inserisci il numero {i + 1}:");
+                do
+                {
+                    control = int.TryParse(Console.ReadLine(), out numeri[i]);
+                    if (!control)
+                    {
+                        Console.WriteLine("Errore: inserisci un numero intero.");
+                    }
+                }
+                while (!control);
+            }
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (numeri[i] > 0 && numeri[i+1] < 0)
+                {
+                    cont++;
+                }
+                else if (numeri[i] < 0 && numeri[i + 1] > 0)
+                {
+                    cont++;
+                }
+            }
+
+            Console.WriteLine($"Il numero di cambi di segno è: {cont}");
+        }
+    }
+}
