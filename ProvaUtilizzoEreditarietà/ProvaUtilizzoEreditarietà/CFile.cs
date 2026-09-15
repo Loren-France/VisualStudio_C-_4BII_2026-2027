6470b10d4598e7436562ebc7fbe3ff926e31d656
Loren-France <14529846@itisrossi.vi.it>
2026-09-15 19:18:01 +0200

    Primo utilizzo dell'ereditarietà

---
+++ ProvaUtilizzoEreditarietà/ProvaUtilizzoEreditarietà/CFile.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProvaUtilizzoEreditarietà
+{
+    public class CFile
+    {
+        private string estensione;
+        private double dimensione;
+
+        public string Estensione
+        {
+            get { return estensione; }
+            set { estensione = value; }
+        }
+
+        public double Dimensione
+        {
+            get { return dimensione; }
+            set { dimensione = value; }
+        }
+
+        public CFile(string est, double dim)
+        {
+            estensione = est;
+            dimensione = dim;
+        }
+        public string Apri()
+        {
+            return "File Aperto";
+        }
+    }
+
+    public class CAudio : CFile
+    {
+        public int Volume { get; set; }
+        public CAudio() : base(".mp3", 3.5) { Volume = 100; }
+
+        public string Riproduci()
+        {
+            return "Sto riproducendo l’audio.";
+        }
+    }
+
+    public class CImmagine : CFile
+    {
+        public int Pixel { get; set; }
+        public CImmagine() : base(".jpg", 5.2) { Pixel = 1; }
+
+        public string FiltraBiancoNero()
+        {
+            return "Ho applicato il filtro bianco e nero all'immagine.";
+        }
+    }
+
+    public class CVideo : CFile
+    {
+        public float Durata { get; set; }
+        public CVideo() : base(".mp4", 25.0) { Durata = 3.5f; } //Problema con tipologia di input, impossibile mettere un float se con con 'f' alla fine, altrimenti viene considerato un double e da errore.
+
+        public string Visualizza()
+        {
+            return "Sto visualizzando il video.";
+        }
+    }
+}
---
+++ ProvaUtilizzoEreditarietà/ProvaUtilizzoEreditarietà/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ProvaUtilizzoEreditarietà
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CAudio mioAudio = new CAudio();
+            CVideo mioVideo = new CVideo();
+            CImmagine miaImmagine = new CImmagine();
+
+            Console.WriteLine("Audio");
+            Console.WriteLine("Estensione: " + mioAudio.Estensione);
+            Console.WriteLine("Dimensione: " + mioAudio.Dimensione);
+            Console.WriteLine("Volume: " + mioAudio.Volume);
+            Console.WriteLine(mioAudio.Riproduci());
+
+            Console.WriteLine("\nVideo");
+            Console.WriteLine("Estensione: " + mioVideo.Estensione);
+            Console.WriteLine("Dimensione: " + mioVideo.Dimensione);
+            Console.WriteLine("Durata: " + mioVideo.Durata);
+            Console.WriteLine(mioVideo.Visualizza());
+
+            Console.WriteLine("\nImmagine");
+            Console.WriteLine("Estensione: " + miaImmagine.Estensione);
+            Console.WriteLine("Dimensione: " + miaImmagine.Dimensione);
+            Console.WriteLine("Pixel: " + miaImmagine.Pixel);
+            Console.WriteLine(miaImmagine.FiltraBiancoNero());
+
+        }
+    }
+}
